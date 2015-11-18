d392a8c2693031dd61df6bfdb3a84caddf590a31
Lior Shefer <lior.shefer@wix.com>
2015-11-18 16:21:57 +0200

    new structure again

--- assets/plugins/wixcode.js
+++
@@ -1,9 +0,0 @@
-exports.handlers = {
-    newDoclet: function(e) {
-        if(e.doclet && e.doclet.product) {
-            if(e.doclet.product !== 'code') {
-                e.doclet.ignore = true;
-            }
-        }
-    }
-};
